13ccc339142c5a9917f44a668ea46192836579bb
CowNow <2095721273@qq.com>
2024-11-05 09:00:46 +0000

    多线程运行
    Signed-off-by: CowNow <2095721273@qq.com>

--- CS2/Main.py
+++ CS2/Main.py
@@ -1,6 +1,15 @@
+import threading
+
 from ESP import Esp
+import gui
 
 esp = Esp()
 
-if __name__ == '__main__':
-    esp.run()
+gui_thread = threading.Thread(target=gui.render)
+esp_thread = threading.Thread(target=esp.run)
+
+gui_thread.start()
+esp_thread.start()
+
+gui_thread.join()
+esp_thread.join()
